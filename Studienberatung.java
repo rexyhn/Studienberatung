93c5c27dd2f33bbc299206f2839797547a140fba
rexyhn <rexyhn3@googlemail.com>
2017-04-18 16:06:33 +0200

    Create Studienberatung.java

---
+++ Studienberatung.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Studienberatung {
+
+	public static void main(String[] args) {
+
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("\n\tHast du ein Abitur(j/n): ");
+		String a = scanner.next();
+		
+		
+		if(a.equals("j")){
+			System.out.print("\n\tBist du wissbegierig?(j/n)?: ");
+
+			a = scanner.next();
+				if(a.equals("j")){
+				System.out.print("\n\tAuf jeden Fall studieren.");
+				}
+				else if(a.equals("n")){
+				System.out.print("\n\tNicht studieren.");
+				}
+		}
+		
+		else if(a.equals("n")){
+			
+			System.out.print("\n\tHast du ein Fachabitur(j/n)?: ");
+			a = scanner.next();
+				if(a.equals("j")){
+					
+					System.out.print("\n\tHast du eine Berufsausbildung(j/n)?: ");
+					a = scanner.next();
+						if(a.equals("j")){
+							
+							System.out.print("\n\tWillst du sofort Geld verdienen(j/n)?: ");
+							a = scanner.next();
+								if(a.equals("j")){
+									System.out.print("\n\tDu kannst spaeter noch studieren.");
+								}
+								else if(a.equals("n")){
+									System.out.print("\n\tDann studier doch.");
+								}
+						}
+						else if(a.equals("n")){
+							System.out.print("\n\tBesser noch studieren.");
+						}
+				}
+				else if(a.equals("n")) {
+				System.out.print("\n\tNicht studieren.");
+				}
+		}
+	}
+	
+	}
+
